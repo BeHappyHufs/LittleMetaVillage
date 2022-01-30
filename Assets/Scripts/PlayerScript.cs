c62a603f481bca29dc16335449b4c7520d1a7dcc
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-01-30 23:12:24 +0900

    Feat : "로그인"
    Script 수정

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,9 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
 using Cinemachine;
+using MySql.Data.MySqlClient;
+using System;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerScript :  MonoBehaviourPunCallbacks, IPunObservable
@@ -24,6 +27,15 @@ public class PlayerScript :  MonoBehaviourPunCallbacks, IPunObservable
     public Text NickNameText;
 
     public PhotonView PV;
+
+    public InputField id;
+    public InputField password;
+    public InputField face;
+    public InputField hair;
+    public InputField body;
+    public InputField kit;
+    public GameObject upBut;
+    public GameObject failState;
 
 
     void Start()
@@ -45,6 +57,46 @@ public class PlayerScript :  MonoBehaviourPunCallbacks, IPunObservable
             CM.LookAt = transform;
         } 
 
+    }
+
+    public void chageSecne()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+
+    public void up()
+    {
+        using (MySqlConnection connection = new MySqlConnection("Server=us-cdbr-east-04.cleardb.com;Port=3306;Database=heroku_9739fee54be3ce9;Uid=b30ac1742c1d56;Pwd=6db3d0ba"))
+        {
+            upBut.SetActive(false);
+            failState.SetActive(false);
+            string insertQ = "INSERT INTO avatar(id,body,face,hair,kit,password) VALUES('" + id.text + "', " + body.text + ", " + face.text + ", " + hair.text + ", " + kit.text + ", " + password.text + ")";
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(insertQ, connection);
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    upBut.SetActive(true);
+                    failState.SetActive(false);
+                    Debug.Log("가입 성공");
+                    Debug.Log(insertQ);
+                }
+                else
+                {
+                    failState.SetActive(true);
+                    upBut.SetActive(false);
+                    Debug.Log("가입 실패");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("완전 실패");
+                Debug.Log(insertQ);
+                Debug.Log(ex.ToString());
+            }
+        }
     }
 
     // Update is called once per frame
