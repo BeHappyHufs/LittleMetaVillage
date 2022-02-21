9e8b4d092761c1fe6b3b9b284bdee10ac66d53c7
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-02-21 19:12:47 +0900

    Feat : 씬이동 기능 추가
    - 씬이동 기능 추가

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+  public void ChangeFirstScene()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+
+    public void ChangeSecondScene()
+    {
+        SceneManager.LoadScene("SecondScene");
+    }
+
+
+
+}
