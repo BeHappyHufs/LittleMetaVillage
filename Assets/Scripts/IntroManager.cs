1fdfcf199f207585939972d2654c0c2272376bc6
s-rachmaninoff <campanella07@naver.com>
2022-07-07 05:09:06 +0900

    Add : "인트로 씬 추가" 인트로 로고 에니메이션 추가

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    public string SceneToLoad;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Demo");
+        }
+
+    }
+}
